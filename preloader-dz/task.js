783b5435b5cc2aa91be9e796dd3f1fca132ada7d
Alexsander <sparco@nxt.ru>
2024-04-08 16:07:32 +0500

    preloader dz done

---
+++ preloader-dz/task.js
@@ -0,0 +1,25 @@
+const xhr = new XMLHttpRequest()
+const loader = document.querySelector('.loader')
+const items = document.getElementById('items')
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses')
+xhr.addEventListener('readystatechange', function () {
+    if(xhr.readyState === xhr.DONE) {
+        const data = JSON.parse(xhr.responseText)
+        const currencies = data['response']['Valute']
+        for (const currency in currencies ) {
+            const currencyElement = currencies[currency]
+            const currencyValue = currencyElement.Value
+            const currencyCharCode = currencyElement.CharCode
+            const newDiv = document.createElement('div')
+            newDiv.className = 'item'
+            items.appendChild(newDiv)
+            newDiv.insertAdjacentHTML('beforeend', `<div class="item__code">${currencyCharCode}</div><div class="item__value">${currencyValue}</div><div class="item__currency">руб.</div>`)
+        }
+        loader.classList.remove('loader_active')
+
+
+
+    }
+})
+
+xhr.send()
